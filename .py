3f7a5e2a661414da838020ecf6a7420ab3d7adea
new-script0 <newscript0g@gmail.com>
2025-02-14 21:04:41 +0300

    Update .py

--- .py
+++ .py
@@ -3,16 +3,16 @@ import pystyle
 default_print = print
 default_input = input
 def print(text : str):
-  default_print(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
+    default_print(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
 def input(text : str = "") -> str:
-  default_input(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
+    default_input(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
 
 def main():
-  print("""███    ██ ███████ ██     ██         ███████  ██████ ██████  ██ ██████  ████████  ██████
+    print("""███    ██ ███████ ██     ██         ███████  ██████ ██████  ██ ██████  ████████  ██████
 ████   ██ ██      ██     ██         ██      ██      ██   ██ ██ ██   ██    ██    ██  ████
 ██ ██  ██ █████   ██  █  ██         ███████ ██      ██████  ██ ██████     ██    ██ ██ ██
 ██  ██ ██ ██      ██ ███ ██              ██ ██      ██   ██ ██ ██         ██    ████  ██
 ██   ████ ███████  ███ ███  ███████ ███████  ██████ ██   ██ ██ ██         ██     ██████""")
-  input()
+    input()
 
 main()
