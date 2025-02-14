bef163bfa8ba921e3022e74a325d7cf3ac8616d6
new-script0 <newscript0g@gmail.com>
2025-02-14 21:00:17 +0300

    Update .py

--- .py
+++ .py
@@ -4,7 +4,7 @@ default_print = print
 default_input = input
 def print(text : str):
   default_print(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
-def input(text : str) -> str:
+def input(text : str = "") -> str:
   default_input(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
 
 def main():
