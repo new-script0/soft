0d256ff6b0e7d24e457ec851fe3852bfcf4b4f15
new-script0 <newscript0g@gmail.com>
2025-02-14 20:58:57 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,18 @@
+import pystyle
+
+default_print = print
+default_input = input
+def print(text : str):
+  default_print(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
+def input(text : str) -> str:
+  default_input(pystyle.Center.XCenter(pystyle.Colorate.Horizontal(pystyle.Colors.cyan_to_green, text)))
+
+def main():
+  print("""███    ██ ███████ ██     ██         ███████  ██████ ██████  ██ ██████  ████████  ██████
+████   ██ ██      ██     ██         ██      ██      ██   ██ ██ ██   ██    ██    ██  ████
+██ ██  ██ █████   ██  █  ██         ███████ ██      ██████  ██ ██████     ██    ██ ██ ██
+██  ██ ██ ██      ██ ███ ██              ██ ██      ██   ██ ██ ██         ██    ████  ██
+██   ████ ███████  ███ ███  ███████ ███████  ██████ ██   ██ ██ ██         ██     ██████""")
+  input()
+
+main()
